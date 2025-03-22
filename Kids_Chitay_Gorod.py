a189ffa56ba5c571face85c02e9da33c8ba9d2a9
AlexShayko <alex.shayko04@gmail.com>
2025-03-22 14:25:37 +0300

    Other Parsed Sites

---
+++ Kids_Chitay_Gorod.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
+import undetected_chromedriver as uc
+import numpy as np
+from Chitay_Gorod_Fantasy import create_driver, parse_books_Chitay_Gorod
+
+url = 'https://www.chitai-gorod.ru/catalog/books/detskie-knigi-110090?page=1&filters%5BratingStars%5D=4&filters%5BratingStars%5D=5'
+
+driver = create_driver(False)
+
+driver.get(url)
+
+prepared_books = parse_books_Chitay_Gorod(driver)
+
+df_Chitay_Gorod_kids = pd.DataFrame(prepared_books, columns=['Book_Title', 'Author', 'Publisher', 'Age_Restriction', 'Rating', 'Price(in Rubles)'])
+
+df_Chitay_Gorod_kids.to_csv('books_kids_Chitay_Gorod.csv', index=False)
